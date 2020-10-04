fbf1fbff1c943aec181627430d5e24641e09160a
Julio Della Flora <jcldf@hotmail.com>
2020-10-03 22:54:21 -0300

    Update hardware aimbot.ino

--- hardware aimbot.ino
+++ hardware aimbot.ino
@@ -1,25 +1,3 @@
-//
-// begin license header
-//
-// This file is part of Pixy CMUcam5 or "Pixy" for short
-//
-// All Pixy source code is provided under the terms of the
-// GNU General Public License v2 (http://www.gnu.org/licenses/gpl-2.0.html).
-// Those wishing to use Pixy source code, software and/or
-// technologies under different licensing terms should contact us at
-// cmucam@cs.cmu.edu. Such licensing terms are available for
-// all portions of the Pixy codebase presented here.
-//
-// end license header
-//
-// This sketch is a good place to start if you're just getting started with 
-// Pixy and Arduino.  This program simply prints the detected object blocks 
-// (including color codes) through the serial console.  It uses the Arduino's 
-// ICSP SPI port.  For more information go here:
-//
-// https://docs.pixycam.com/wiki/doku.php?id=wiki:v2:hooking_up_pixy_to_a_microcontroller_-28like_an_arduino-29
-//
-  
 #include <Pixy2.h>
 #include <SPI.h>
 #include <Mouse.h>
@@ -58,12 +36,17 @@ void loop()
       Serial.print(width); 
       Serial.print(" "); 
       Serial.println(height);
-      int x = map(width, 0, 315, 0, 1920);
-      int y = map(height, 0, 207, 0, 1080); 
+      //int x = map(width, 157, -157, 0, 315);
+      //int y = map(height, 103, -103, 0, 207); 
+      
+      int x = map(width, 0, 315, -157, 157);
+      int y = map(height, 0, 207, -103, 103);
+     
       Serial.print(x);
       Serial.print(" ");
       Serial.println(y);
-      delay(100);
+      Mouse.move(x, y);
+      delay(5);
     }
   }  
 }
